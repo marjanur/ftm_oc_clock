8315fb06bb299b4924453cfdcd21bbc1fbbbacfe
marjanur <40625183+marjanur@users.noreply.github.com>
2019-10-07 05:53:38 +1100

    Final

---
+++ Final Full Project/Debug/Sources/fonts.d
@@ -0,0 +1,4 @@
+Sources/fonts.o Sources/fonts.o: ../Sources/fonts.cpp \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Sources/fonts.h
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Sources/fonts.h:
---
+++ Final Full Project/Debug/Startup_Code/security.d
@@ -0,0 +1,26 @@
+Startup_Code/security.o Startup_Code/security.o: \
+ ../Startup_Code/security.c \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/derivative.h \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/MK20D5.h \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cm4.h \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cmInstr.h \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/cmsis_gcc.h \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cmFunc.h \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cmSimd.h \
+ C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/system.h
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/derivative.h:
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/MK20D5.h:
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cm4.h:
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cmInstr.h:
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/cmsis_gcc.h:
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cmFunc.h:
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/core_cmSimd.h:
+
+C:/Users/Marjanur\ Rahman/Documents/workspace/tute_5_q3/Project_Headers/system.h:
